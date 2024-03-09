d7871b1e81d99776789e9ab888582d10abe8cc21
Daniela Biolek <daniela.biolek@centrum.cz>
2024-03-09 15:39:47 +0100

    opravatrim

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const jmeno = prompt("Zadej jméno bez diakritiky!")
-const prijmeni = prompt("Zadej příjmení bez diakritiky")
+const jmeno = prompt("Zadej jméno bez diakritiky:")
+const prijmeni = prompt("Zadej příjmení bez diakritiky:")
 
-const jmenoinicialy = jmeno.slice(0,3).toLowerCase().trim()
-const prijminicialy = prijmeni.slice(0,4).toLowerCase().trim()
+const jmenoinicialy = jmeno.trim().slice(0,3).toLowerCase()
+const prijmenicialy = prijmeni.trim().slice(0,4).toLowerCase()
 
-const email = prijminicialy + jmenoinicialy +"@fit.cvut.cz"
+const email = prijmenicialy + jmenoinicialy +"@fit.cvut.cz"
 
 document.body.innerHTML = email
